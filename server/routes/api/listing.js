20a20d7afaebca15f3a20d27d2bd4a2d684f53c9
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-05-11 17:13:43 -0400

    much nicer code for checking parameters and adding props

--- server/routes/api/listing.js
+++ server/routes/api/listing.js
@@ -5,32 +5,32 @@ let jwtProcess = require('../../jwt');
 const User = require('../../models/User')
 
 module.exports = (app) => {
-        // Create Listing
     app.post('/api/listing/create', (req,res,next) => {
         const { body } = req; 
-        const {
-            seller,
-            name,
-            description,
-            price,
-            endtime,
-            status,
-            image,
-            friendDiscount
-        } = body; 
-        // console.log(userID, name, description, price, endtime, status, image, friendDiscount);
 
         const newListing = new Listing();
-        newListing.seller = seller;
-        newListing.name = name;
-        newListing.description = description;
-        newListing.price = price;
-        newListing.endtime = endtime;
-        newListing.status = status;
-        newListing.image = image;
-        if(friendDiscount)
-            newListing.friendDiscount = friendDiscount;
-        // newListing.friendDiscount = friendDiscount;
+
+        props = [
+            'seller' ,
+             'name', 
+             'description', 
+             'price', 
+             'endtime', 
+             'status', 
+             'image', 
+             'friendDiscount'];
+
+        props.forEach(prop => {
+            console.log(body[prop])
+            if(body[prop] == null) {
+                return res.send({
+                    success: false,
+                    message: "missing: " + prop,
+                }); 
+            } else {
+                newListing[prop] = body[prop]
+            } 
+        })
         newListing.save((err, listing) => {
             if (err) {
                 return res.send({
@@ -45,10 +45,5 @@ module.exports = (app) => {
                 message: "Listing created."
             });
         })
-
-        // return res.send({
-        //     success: "idk",
-        //     message: "we'll see..."
-        // })
     });
 }
