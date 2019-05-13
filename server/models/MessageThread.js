cb634fee71eb812ff9a204dfd5f40e4e3455d501
erickolbusz <erickolbusz@gmail.com>
2019-05-13 17:23:35 -0400

    gitignore update because im dumb, also schema merge

--- server/models/MessageThread.js
+++ server/models/MessageThread.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
+const MessageSchema = new mongoose.Schema({ 
+	id : {
+		type: Number,
+		required: true
+	},
+	sender : {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+		required: true
+	},
+	date : {
+		type: Date,
+		default: Date.now
+	},
+	content : {
+		type: String,
+		required: true
+	}
+	isDelete : {
+		type: Boolean,
+		default: false
+	}
+});
+
 const MessageThreadSchema = new mongoose.Schema({ 
 	id : {
 		type: Number,
@@ -9,10 +33,7 @@ const MessageThreadSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User'
 	}],
-	messages : [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Message'
-	}]
+	messages : [MessageSchema]
 });
 
 module.exports = mongoose.model('MessageThread', MessageThreadSchema);
