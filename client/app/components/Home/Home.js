ad085bf7c1ac42f02132faa81f47cc2fbde5681b
Nabil Ahmed Khatri <nabil@nabilahmed.com>
2019-05-13 14:15:26 -0400

    "adding state"

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -5,89 +5,35 @@ import { Container, Grid, Row, Col } from 'react-bootstrap';
 import SearchBar from '../Search/Search'
 import Recommendation from '../Recommendation/Recommendation'
 import Listing from '../Listing/Listing'
+import { getJwt } from '../../helpers/getjwt'
+import Axios from 'axios'
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      counters: []
+      userName: '',
     };
-
-    this.newCounter = this.newCounter.bind(this);
-    this.incrementCounter = this.incrementCounter.bind(this);
-    this.decrementCounter = this.decrementCounter.bind(this);
-    this.deleteCounter = this.deleteCounter.bind(this);
-
-    this._modifyCounter = this._modifyCounter.bind(this);
   }
 
   componentDidMount() {
-    fetch('/api/counters')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          counters: json
-        });
-      });
+    const jwt = getJwt();
+    console.log(jwt)
+    if (jwt) {
+    Axios.get('/verify', {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    }).then(res => {
+      console.log(res); 
+    }).catch(err => {
+        this.props.history.push('/Login')
+        localStorage.removeItem('cool-jwt')
+      }
+      )
   }
-
-  newCounter() {
-    fetch('/api/counters', { method: 'POST' })
-      .then(res => res.json())
-      .then(json => {
-        let data = this.state.counters;
-        data.push(json);
-
-        this.setState({
-          counters: data
-        });
-      });
-  }
-
-  incrementCounter(index) {
-    const id = this.state.counters[index]._id;
-
-    fetch(`/api/counters/${id}/increment`, { method: 'PUT' })
-      .then(res => res.json())
-      .then(json => {
-        this._modifyCounter(index, json);
-      });
-  }
-
-  decrementCounter(index) {
-    const id = this.state.counters[index]._id;
-
-    fetch(`/api/counters/${id}/decrement`, { method: 'PUT' })
-      .then(res => res.json())
-      .then(json => {
-        this._modifyCounter(index, json);
-      });
-  }
-
-  deleteCounter(index) {
-    const id = this.state.counters[index]._id;
-
-    fetch(`/api/counters/${id}`, { method: 'DELETE' })
-      .then(_ => {
-        this._modifyCounter(index, null);
-      });
-  }
-
-  _modifyCounter(index, data) {
-    let prevData = this.state.counters;
-
-    if (data) {
-      prevData[index] = data;
-    } else {
-      prevData.splice(index, 1);
-    }
-
-    this.setState({
-      counters: prevData
-    });
-  }
-
+}
 
   render() {
     let title = {
@@ -97,19 +43,19 @@ class Home extends Component {
       color: '#007bff',
       weight: 'Bold'
     }
-
+    
     return (
-    <Container>
-      <Row>
-        <h1 style={title}>BidBuySell</h1>
-      </Row>
-      <Row>
-        <SearchBar></SearchBar>
-      </Row>
-      <Row>
-        <Recommendation></Recommendation>
-      </Row>
-    </Container>
+      <Container>
+        <Row>
+          <h1 style={title}>BidBuySell</h1>
+        </Row>
+        <Row>
+          <SearchBar></SearchBar>
+        </Row>
+        <Row>
+          <Recommendation></Recommendation>
+        </Row>
+      </Container>
 
     );
 
--- client/app/components/Login/Login.js
+++ client/app/components/Login/Login.js
@@ -1,40 +1,100 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Alert, Container } from 'react-bootstrap'
 import '../../styles/login.css'
 import { Link } from 'react-router-dom';
 
 
 class Login extends Component {
-    render() {
-        return (
-            <div className="login-box">
-            <Form className="login-form">
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: '',
+      password: '',
+      loginAlert: false,
+      loginAlertMessage: '',
+      loginAlertVariant: ''
+    }
+  }
+
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submit = e => {
+    e.preventDefault();
+    axios.post('/api/account/signin', {
+      userName: this.state.userName,
+      password: this.state.password
+    }).then(res => {
+      switch (res.data.success) {
+        case false:
+          this.setState({
+            loginAlert: true,
+            loginAlertMessage: res.data.message,
+            loginAlertVariant: 'danger'
+          })
+          break;
+        case true:
+          localStorage.setItem('cool-jwt', res.data.token);
+          this.setState({
+            loginAlert: true,
+            loginAlertMessage: res.data.message,
+            loginAlertVariant: 'success'
+          })
+          this.props.history.push('/myaccount')
+          break;
+      }
+    })
+  }
+
+  render() {
+    const {
+      userName,
+      password,
+      loginAlert,
+      loginAlertMessage,
+      loginAlertVariant
+    } = this.state;
+
+    const handleHide = () => this.setState({ loginAlert: false })
+
+    return (
+      <Container>
+        <div className="login-box">
+          <Form onSubmit={this.submit} className="login-form">
             <h2>Log in</h2>
+            <Alert onClose={handleHide} show={loginAlert} dismissible variant={loginAlertVariant}>
+                {loginAlertMessage}
+            </Alert>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" name ="user" />
+              <Form.Control type="text" placeholder="Enter username" name="userName" onChange={this.change} />
             </Form.Group>
-          
+
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" name="password" />
+              <Form.Control type="password" placeholder="Password" name="password" onChange={this.change} />
             </Form.Group>
             <Button className="mb-3" variant="primary" type="submit">
               Submit
             </Button>
-            
-            <h5>Don't have an account? Submit an application!</h5>
-                <Link to="/apply"><Button variant="primary" type="submit">
+
+            <h5>Don't have an account? Submit an sapplication!</h5>
+            <Link to="/apply"><Button variant="primary" type="submit">
               Apply
             </Button></Link>
-        
+
           </Form>
-          </div>
-        )
-    }
+        </div>
+      </Container>
+    )
+  }
 }
 
 export default Login; 
