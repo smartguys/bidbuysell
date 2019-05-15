2f9e68b2e171308c0e1a24c4f577a80f177ca606
Nabil Ahmed Khatri <nabil@nabilahmed.com>
2019-05-14 23:47:24 -0400

    "fixed listing page and timer"

--- client/app/components/Listing/Listing.js
+++ client/app/components/Listing/Listing.js
@@ -5,37 +5,85 @@ import CurrentPrice from './CurrentPrice';
 import BidAmount from './BidAmount';
 import SellerInfo from './SellerInfo';
 import SearchBar from '../Search/Search'
+import axios from 'axios'
+import Countdown from 'react-countdown-now';
 
 class Listing extends React.Component {
     constructor(props) {
         super(props);
         console.log(this.props.match.params.id)
+
+        this.state = ({
+            listing: {},
+            bids: []
+        })
+    }
+
+    componentDidMount() {
+        this.getListing()
+    }
+
+    getListing = () => {
+        let id = this.props.match.params.id
+        axios.get(`/api/listing/id/${id}`).then(res => {
+            switch (res.data.success) {
+                case false:
+                    break;
+                case true:
+                    console.log(res.data)
+                    this.setState({
+                        listing: res.data.data.listing,
+                        bids: res.data.data.bids
+                    })
+                    break;
+            }
+        })
     }
 
     render() {
+        let {
+            listing,
+            bids
+        } = this.state;
+        let endtime = new Date(listing.endtime)
+
+        const renderer = ({ days, hours, minutes, seconds, completed }) => {
+            if (completed) {
+              // Render a completed state
+              return "Done"
+            } else {
+              // Render a countdown
+              return <span>{days} days, {hours} hours, {minutes} minutes, {seconds}, seconds</span>;
+            }
+          };
+
         return (
             <Container fluid={true}>
                 <Row className="mt-3">
                     <Col xs lg="8"><SearchBar></SearchBar></Col>
                     <Col></Col>
                 </Row>
-                {/* <Row>
-                    <Col><Product></Product>
+                <Row className="mt-3">
+
+                    <Col>
+                        <Product listing={listing}></Product>
                     </Col>
 
                     <Col>
-                        <Row><SellerInfo></SellerInfo>
+                        <Row>
+                            <SellerInfo seller={listing.seller}></SellerInfo>
                         </Row>
-                        
-                        <Row><CurrentPrice></CurrentPrice>
-                        </Row>    
-                        
-                        <Row><BidAmount></BidAmount>                       
+
+                        <Row className="mt-3">
+                            <h5>Time Remaining</h5>
                         </Row>
-                 
-                    </Col>              
-              
-            </Row> */}
+                        <Row><Countdown renderer={renderer} date={endtime}>
+                            </Countdown></Row>
+                        <Row className="mt-3">
+                            <BidAmount listing={listing}></BidAmount>
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
         )
     }
